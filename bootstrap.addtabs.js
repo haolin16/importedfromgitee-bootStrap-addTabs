7e964c8782a098e4c9b603e3b3a877c0acb82b29
自由自在的小鱼 <hbbcs@sina.com>
2017-08-21 09:02:19 +0800

    更新 bootstrap.addtabs.js

--- bootstrap.addtabs.js
+++ bootstrap.addtabs.js
@@ -13,7 +13,7 @@
         close: true, //是否可以关闭
         monitor: 'body', //监视的区域
         iframe: false, //使用iframe还是ajax
-        iframeHeight: $(document).height() - 100, //固定TAB中IFRAME高度,根据需要自己修改
+        iframeHeight: $(window).height() - 100, //固定TAB中IFRAME高度,根据需要自己修改
         target: '.nav-tabs',
         loadbar: true,
         contextmenu: true, //是否使用右键菜单
